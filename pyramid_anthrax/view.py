4b8e1d6a5298fd71d70f01093c1bec24d746908d
Szymon Pyżalski <szymon.pyzalski@stxnext.pl>
2012-07-21 14:52:02 +0200

    Overcomplicated but working solution

--- pyramid_anthrax/view.py
+++ pyramid_anthrax/view.py
@@ -1,4 +1,7 @@
+import sys
+
 import pyramid.view 
+from venusian.advice import getFrameInfo
 
 def view_config(*args, **kwargs):
     Form = kwargs.pop('Form', None)
@@ -15,11 +18,15 @@ def view_config(*args, **kwargs):
             form.__raw__ = request.POST
             request.form = form
             if not form.__valid__ and on_invalid is not None:
-                import pdb; pdb.set_trace()
                 return pyramid.view.render_view_to_response(
                     context=request.context, request=request, name=on_invalid)
             else: 
                 return orig_view(*view_args)
 
-        return pyramid.view.view_config(*args, **kwargs)(view)
+        wrapped = pyramid.view.view_config(*args, **kwargs)(view)
+        venusian_callbacks = wrapped.__venusian_callbacks__['pyramid'][0]
+        module = getFrameInfo(sys._getframe(1))[1]
+        wrapped.__venusian_callbacks__['pyramid'] = [(
+            venusian_callbacks[0], module.__name__)]
+        return wrapped
     return view_config
